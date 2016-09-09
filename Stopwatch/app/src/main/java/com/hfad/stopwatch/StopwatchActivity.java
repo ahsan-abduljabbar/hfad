11be656ec1dc9ab9c84468044611fcfeb57a972c
Ahsan Abdul Jabbar <ahsan.abduljabbar@outlook.com>
2016-09-09 16:59:04 +0500

    HFAD Learning

--- Stopwatch/app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
+++ Stopwatch/app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
@@ -1,6 +1,7 @@
 package com.hfad.stopwatch;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class StopwatchActivity extends AppCompatActivity {
 
@@ -50,11 +52,50 @@ public class StopwatchActivity extends AppCompatActivity {
 
     }
 
+    private void runTimer () {
+
+        final TextView timeView = (TextView) findViewById(R.id.time_view);
+
+        final Handler handler = new Handler();
+
+        handler.post(new Runnable() {
+
+            @Override
+
+            public void run () {
+
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+
+                String time = String.format("%d:%02d:%02d", hours, minutes, secs);
+
+                timeView.setText(time);
+
+                if(running) {
+
+                    seconds++;
+
+                }
+
+                handler.postDelayed(this, 1000);
+
+            }
+
+        });
+
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_stopwatch, menu);
+
+        runTimer();
+
         return true;
+
     }
 
     @Override
