91a6b5a017e857afd139cbe473a80f8c05d759ca
Ahsan Abdul Jabbar <ahsan.abduljabbar@outlook.com>
2016-12-06 18:21:05 +0500

    Learning

--- Stopwatch/app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
+++ Stopwatch/app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
@@ -1,5 +1,6 @@
 package com.hfad.stopwatch;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
@@ -93,6 +94,18 @@ public class StopwatchActivity extends AppCompatActivity {
         });
 
     }
+
+    //
+
+    public void onShowList (View view) {
+
+        Intent intent = new Intent(this, ListActivity.class);
+
+        startActivity(intent);
+
+    }
+
+    //
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
